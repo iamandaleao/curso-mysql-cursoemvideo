bf16fbf9593dc8f9b233133ae57dc64e1288a9ec
iamandaleao <amandabibiano144@gmail.com>
2025-07-24 17:41:49 -0300

    docs: create-db

---
+++ criar-banco.sql
@@ -0,0 +1,7 @@
+-- CRIAR BANCO
+-- CREATE DATABASE cadastro: cria um novo banco de dados chamado "cadastro"
+-- DEFAULT CHARACTER SET utf8: define o uso do conjunto de caracteres UTF-8
+-- DEFAULT COLLATE utf8_general_ci: define comparações que ignoram maiúsculas e minúsculas
+CREATE DATABASE cadastro
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
