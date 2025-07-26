7b2ddc2c03afafe44e9435af1481a44c67cf7a29
iamandaleao <amandabibiano144@gmail.com>
2025-07-25 22:39:13 -0300

    docs: truncate

---
+++ truncate.sql
@@ -0,0 +1,8 @@
+-- TRUNCATE (apaga todos os registros da tabela, mas mantém a estrutura)
+
+TRUNCATE TABLE cursos;
+-- ou
+TRUNCATE cursos;
+
+TRUNCATE TABLE cursos;
+-- Também pode ser usado assim: TRUNCATE cursos;
