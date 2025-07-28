f1a954d833ac144483dceee545bb708338769894
iamandaleao <amandabibiano144@gmail.com>
2025-07-28 11:31:11 -0300

    feat: add select distinct

---
+++ select-distinct.sql
@@ -0,0 +1,8 @@
+-- ========================
+-- REMOVER REPETIÇÕES COM DISTINCT
+-- ========================
+
+-- Retorna apenas valores únicos da coluna "carga"
+SELECT DISTINCT carga FROM cursos;
+
+
