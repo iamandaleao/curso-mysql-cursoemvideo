1e5863ec977aa18a6f8cf6f2705e335a961390ad
iamandaleao <amandabibiano144@gmail.com>
2025-07-30 10:24:42 -0300

    feat: add SQL exercises

---
+++ exercicios/1.sql
@@ -0,0 +1,30 @@
+--- Uma lista com o nome de todas as gafanhotas
+
+select * from gafanhotos where sexo = 'F';
+
+--- Resultados
+
+'Ana Carolina Hernandes'
+'Ana Carolina Mendes'
+'Andreia Delfino'
+'Bruna Santos'
+'Bruna Teles'
+'Daniele Moledo'
+'Dayana Dias'
+'Elaine Nunes'
+'Janaina Oliveira'
+'Janaína Couto'
+'Jarismar Andrade'
+'Josiane Dutra'
+'Jucinei Teixeira'
+'Karine Ribeiro'
+'Leila Martins'
+'Letícia Neves'
+'Monique Precivalli'
+'Nara Matos'
+'Rita Pontes'
+'Rosana Kunz'
+'Silvana Albuquerque'
+'Talita Nascimento'
+'Teresa'
+
