d211760324381d35f67f3d4348cc4b8d8a018001
iamandaleao <amandabibiano144@gmail.com>
2025-07-30 11:03:59 -0300

    feat: add exercises

---
+++ exercises/3.sql
@@ -0,0 +1,16 @@
+-- Lista com id, nome, sexo e profissão de todos os homens que são programadores:
+
+SELECT id, nome, sexo, profissao FROM gafanhotos WHERE sexo = 'M' AND profissao = 'programador';
+
+-- '3', 'Emerson Gabriel', 'M', 'Programador'
+-- '9', 'Jackson Telles', 'M', 'Programador'
+-- '13', 'Allan Silva', 'M', 'Programador'
+-- '25', 'Raian Porto', 'M', 'Programador'
+-- '30', 'Anderson Rafael', 'M', 'Programador'
+-- '35', 'Márcio Mello', 'M', 'Programador'
+-- '40', 'André Schmidt', 'M', 'Programador'
+-- '49', 'Silvio Ricardo', 'M', 'Programador'
+-- '54', 'Andre Santini', 'M', 'Programador'
+-- '55', 'Ruan Valente', 'M', 'Programador'
+-- '58', 'Carlos Camargo', 'M', 'Programador'
+
