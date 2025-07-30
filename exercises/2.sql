3531b673323ba2675599e4268f8c80ccf760e78b
iamandaleao <amandabibiano144@gmail.com>
2025-07-30 10:47:11 -0300

    feat: add exercises

---
+++ exercises/2.sql
@@ -0,0 +1,25 @@
+-- Lista com os dados de todos que nasceram entre 1/jan/2000 e 31/dez/2015
+
+SELECT * FROM gafanhotos WHERE nascimento BETWEEN '2000-01-01' AND '2015-12-31';
+
+
+-- Lista de pessoas que nasceram entre 1/jan/2000 e 31/dez/2015:
+-- '8', 'Carlisson Rosa', 'Professor', '2010-08-01', 'M', '78.22', '1.98', 'Brasil'
+-- '16', 'Elvis Schwarz', 'Dentista', '2011-05-07', 'M', '66.69', '1.76', 'EUA'
+-- '20', 'Marcos Dissotti', 'Empreendedor', '2010-01-01', 'M', '53.79', '1.54', 'Portugal'
+-- '21', 'Ana Carolina Mendes', 'Ator', '2000-12-15', 'F', '88.30', '1.54', 'Brasil'
+-- '22', 'Guilherme de Sousa', 'Dentista', '2001-05-18', 'M', '132.70', '1.97', 'Moçambique'
+-- '23', 'Bruno Torres', 'Auxiliar Administrat', '2000-01-30', 'M', '44.65', '1.65', 'Brasil'
+-- '27', 'Monique Precivalli', 'Auxiliar Administrat', '2013-12-30', 'F', '48.20', '1.22', 'Brasil'
+-- '32', 'Roberto Luiz Debarba', 'Ator', '2007-01-09', 'M', '77.44', '1.56', 'Brasil'
+-- '33', 'Jarismar Andrade', 'Dentista', '2000-06-23', 'F', '63.70', '1.33', 'Brasil'
+-- '35', 'Márcio Mello', 'Programador', '2011-11-20', 'M', '54.11', '1.55', 'EUA'
+-- '36', 'Robson Rodolpho', 'Auxiliar Administrat', '2000-08-08', 'M', '110.10', '1.76', 'Brasil'
+-- '37', 'Daniele Moledo', 'Empreendedor', '2006-08-11', 'F', '101.30', '1.99', 'Brasil'
+-- '39', 'Neriton Dias', 'Auxiliar Administrat', '2009-10-30', 'M', '48.99', '1.29', 'Brasil'
+-- '41', 'Isaias Buscarino', 'Dentista', '2001-01-07', 'M', '99.90', '1.55', 'Moçambique'
+-- '46', 'Diogo Padilha', 'Auxiliar Administrat', '2000-03-03', 'M', '54.34', '1.88', 'Angola'
+-- '49', 'Silvio Ricardo', 'Programador', '2012-03-12', 'M', '65.99', '1.23', 'EUA'
+-- '50', 'Denilson Barbosa da Silva', 'Empreendedor', '2000-01-08', 'M', '97.30', '2.00', 'Brasil'
+-- '58', 'Carlos Camargo', 'Programador', '2005-02-22', 'M', '124.65', '1.33', 'Brasil'
+-- '59', 'Philppe Oliveira', 'Auxiliar Administrat', '2000-05-23', 'M', '105.10', '2.19', 'Brasil'
