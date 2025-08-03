f8d747da4b0fbedff69f8b6022335a9870dec325
iamandaleao <amandabibiano144@gmail.com>
2025-08-03 12:38:39 -0300

    feat: add exercises

--- exercises/9.sql
+++ exercises/9.sql
@@ -1,4 +1,6 @@
 -- Consulta que retorna a quantidade de gafanhotos do sexo feminino com mais de 1.90m de altura
+-- Boa prática: use aspas apenas para valores do tipo texto, não para números
+
 SELECT COUNT(*) 
 FROM gafanhotos 
 WHERE sexo = 'F' AND altura > 1.90;
